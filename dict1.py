04aa0e6ad2610e276f747f924e1c3835b3cb4c37
mounika gajja <42294732+mounikagajja@users.noreply.github.com>
2019-06-11 20:08:05 +0530

    Add files via upload

---
+++ dict1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+animals = {'dogs': [20, 10, 15, 8, 32, 15], 'cats': [3,4,2,8,2,4], 'rabbits': [2, 3, 3], 'fish': [0.3, 0.5, 0.8, 0.3, 1]}
+print(animals['dogs'])
+
+
+print(animals['dogs'][3])
+
+#rint(animals[3])
+
+print(animals['fish'])
